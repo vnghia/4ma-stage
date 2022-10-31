ff5553b70303263986a06901cf98c1d9a72ba9da
Vo Van Nghia <vanvnghia@gmail.com>
2022-10-31 10:39:34 +0100

    report: add big queuing

--- report.r
+++ report.r
@@ -82,7 +82,7 @@ n_visit[n_visit == 0] <- NA
 setNames(melt(
   n_visit[, , 1]
 ), c("Q1", "Q2", "n_visit")) %>%
-  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, n_visit = as.integer(n_visit)) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
   ggplot(aes(Q1, Q2)) +
   geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
   coord_flip() +
@@ -102,7 +102,7 @@ setNames(melt(
 setNames(melt(
   n_visit[, , 2]
 ), c("Q1", "Q2", "n_visit")) %>%
-  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, n_visit = as.integer(n_visit)) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
   ggplot(aes(Q1, Q2)) +
   geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
   coord_flip() +
@@ -381,7 +381,7 @@ n_visit[n_visit == 0] <- NA
 setNames(melt(
   n_visit[, , 1]
 ), c("Q1", "Q2", "n_visit")) %>%
-  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, n_visit = as.integer(n_visit)) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
   ggplot(aes(Q1, Q2)) +
   geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
   coord_flip() +
@@ -401,7 +401,7 @@ setNames(melt(
 setNames(melt(
   n_visit[, , 2]
 ), c("Q1", "Q2", "n_visit")) %>%
-  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, n_visit = as.integer(n_visit)) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
   ggplot(aes(Q1, Q2)) +
   geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
   coord_flip() +
@@ -652,6 +652,179 @@ setNames(melt(
 
 setNames(melt(
   np$load("binary/20x20-queuing-3-v.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  scale_colour_manual(
+    values = c("white", "black", "grey50"),
+    aesthetics = c("fill"), label = c("1", "2", "neutral")
+  )
+
+# ---- 20x20-queuing-4 ----
+
+setNames(melt(
+  np$load("binary/20x20-queuing-4-q-on.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  guides(fill = "none") +
+  scale_colour_manual(values = c("white", "black"), aesthetics = c("fill"))
+
+setNames(melt(
+  np$load("binary/20x20-queuing-4-q-off.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  guides(fill = "none") +
+  scale_colour_manual(values = c("white", "black"), aesthetics = c("fill"))
+
+setNames(melt(
+  np$load("binary/20x20-queuing-4-v.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  scale_colour_manual(
+    values = c("white", "black", "grey50"),
+    aesthetics = c("fill"), label = c("1", "2", "neutral")
+  )
+
+## ---- 20x20-queuing-4-n-visit ----
+
+n_visit <- np$load("binary/20x20-queuing-4-n-visit.npy")
+n_visit[n_visit == 0] <- NA
+
+setNames(melt(
+  n_visit[, , 1]
+), c("Q1", "Q2", "n_visit")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 22) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 22, hjust = 0.5),
+    axis.title.y = element_text(size = 22, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  guides(fill = "none") +
+  scale_fill_scico(palette = "grayC", begin = 0.05, na.value = NA)
+
+setNames(melt(
+  n_visit[, , 2]
+), c("Q1", "Q2", "n_visit")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = n_visit), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 22) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 22, hjust = 0.5),
+    axis.title.y = element_text(size = 22, hjust = 0.5),
+  ) +
+  xlab("$Q_{1}$") +
+  ylab("$Q_{2}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  labs(fill = "Number of \nvisits") +
+  scale_fill_scico(palette = "grayC", begin = 0.05, na.value = NA)
+
+# ---- 20x20-queuing-5 ----
+
+setNames(melt(
+  np$load("binary/20x20-queuing-5-q-on.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  guides(fill = "none") +
+  scale_colour_manual(values = c("white", "black"), aesthetics = c("fill"))
+
+setNames(melt(
+  np$load("binary/20x20-queuing-5-q-off.npy")
+), c("Q1", "Q2", "queue")) %>%
+  mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
+  ggplot(aes(Q1, Q2)) +
+  geom_tile(aes(fill = queue), colour = "grey10", size = 0.001) +
+  coord_flip() +
+  theme_void(base_size = 33) +
+  theme(
+    aspect.ratio = 1,
+    axis.title.x = element_text(size = 33, hjust = 0.5),
+    axis.title.y = element_text(size = 33, hjust = 0.5),
+  ) +
+  ylab("$Q_{2}$") +
+  xlab("$Q_{1}$") +
+  scale_x_reverse(expand = c(0.1, 0.1)) +
+  scale_y_discrete(position = "right", expand = c(0.1, 0.1)) +
+  guides(fill = "none") +
+  scale_colour_manual(values = c("white", "black"), aesthetics = c("fill"))
+
+setNames(melt(
+  np$load("binary/20x20-queuing-5-v.npy")
 ), c("Q1", "Q2", "queue")) %>%
   mutate(Q1 = Q1 - 1, Q2 = Q2 - 1, queue = factor(as.integer(queue) + 1)) %>%
   ggplot(aes(Q1, Q2)) +
